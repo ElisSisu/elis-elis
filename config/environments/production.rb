bf18502ea3e2a3ca02f7c8030bc31cfbd26788e8
Elis Ahmetaj <Elis.Ahmetaj@gmail.com>
2015-01-16 01:05:32 -0500

    Added Amazon web servies S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,3 +80,13 @@ Rails.application.configure do
     # Set this to your actual host place
  config.action_mailer.default_url_options = { :host => 'Rsisu.com' }
 end
+
+#Sets paperclip to uplaod images to Amazon S3 
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
